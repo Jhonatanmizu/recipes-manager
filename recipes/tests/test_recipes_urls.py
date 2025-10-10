2cc6d4fcf3f8a08d2cd097dc14001e58a9de0f39
jhonatanmizu <natanjesuss20@gmail.com>
2025-10-09 21:57:47 -0300

    test: recipes urls

--- recipes/tests/test_recipes_urls.py
+++ recipes/tests/test_recipes_urls.py
@@ -1,26 +1,56 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from recipes.models import Recipe, RecipeCategory
+
 
 class RecipeURLsTestCase(TestCase):
-    def test_recipe_url_is_correct(self):
-        home_url = reverse("recipes:index")
-        self.assertEqual(home_url, "/")
-        response = self.client.get("/")
+    def setUp(self) -> None:
+        self.category = RecipeCategory.objects.create(name="Test Category")
+        self.recipe = Recipe.objects.create(
+            title="Test Recipe",
+            description="Simple description",
+            is_published=True,
+            preparation_time=10,
+            servings=2,
+            preparation_steps="Step 1: Test",
+            preparation_steps_is_html=False,
+            category=self.category,
+        )
+
+    def test_index_url_reverse_and_response(self):
+        url = reverse("recipes:index")
+        self.assertEqual(url, "/")
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "recipes/pages/index.html")
 
-    def test_recipe_category_url_is_correct(self):
-        category_url = reverse("recipes:category", kwargs={"pk": 1})
-        self.assertEqual(category_url, "/recipes/category/1/")
-        response = self.client.get("/recipes/category/1/")
+    def test_category_url_reverse_and_response(self):
+        url = reverse("recipes:category", kwargs={"pk": self.category.pk})
+        expected_path = f"/recipes/category/{self.category.pk}/"
+        self.assertEqual(url, expected_path)
+
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "recipes/pages/category.html")
 
-    def test_recipe_detail_url_is_correct(self):
-        detail_url = reverse("recipes:detail", kwargs={"pk": 2})
-        self.assertEqual(detail_url, "/detail/2/")
+    def test_detail_url_reverse_and_response(self):
+        url = reverse("recipes:detail", kwargs={"pk": self.recipe.pk})
+        expected_path = f"/detail/{self.recipe.pk}/"
+        self.assertEqual(url, expected_path)
 
-    def test_404_recipe_category_url(self):
-        response = self.client.get("recipes/category/99999/")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "recipes/pages/detail.html")
+
+    def test_category_url_returns_404_for_nonexistent_id(self):
+        non_existent_id = self.category.pk + 999
+        url = reverse("recipes:category", kwargs={"pk": non_existent_id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_url_returns_404_for_nonexistent_id(self):
+        non_existent_id = self.recipe.pk + 999
+        url = reverse("recipes:detail", kwargs={"pk": non_existent_id})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
